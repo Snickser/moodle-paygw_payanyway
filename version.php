8de9db439840623949a53e759cd2d30efd5d76f0
Snickser <snickser@gmail.com>
2024-12-12 09:03:55 +0300

    version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024110700;
+$plugin->version   = 2024121200;
 $plugin->requires  = 2023100900;
 $plugin->component = 'paygw_payanyway';
-$plugin->release   = '1.0';
+$plugin->release   = '1.1';
 $plugin->maturity  = MATURITY_STABLE;
